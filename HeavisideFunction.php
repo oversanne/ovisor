3e969fe873b917dbb008087dcd63857f488ad004
Olivier Versanne <oversanne@lafourchette.com>
2014-08-26 00:11:16 +0200

    Add threshold in HeavisideFunction

--- HeavisideFunction.php
+++ HeavisideFunction.php
@@ -3,11 +3,24 @@
 class HeavisideFunction implements FunctionInterface
 {
     /**
+     * @var float
+     */
+    private $threshold;
+
+    /**
+     * @param float $threshold
+     */
+    public function __construct($threshold = 1)
+    {
+        $this->threshold = $threshold;
+    }
+
+    /**
      * @param float $value
      * @return float
      */
     public function evaluate($value)
     {
-        return $value >= 1?1:0;
+        return $value >= $this->threshold?1:0;
     }
 }
